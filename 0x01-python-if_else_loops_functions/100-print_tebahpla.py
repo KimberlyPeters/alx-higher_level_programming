cbf517a56c5f00534c295fac21fece186e16b3ef
KimberlyPeters <kimberlypeters468@gmail.com>
2023-05-09 05:38:41 -0700

    Reverse ASCII string

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 64, -1):
-    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
