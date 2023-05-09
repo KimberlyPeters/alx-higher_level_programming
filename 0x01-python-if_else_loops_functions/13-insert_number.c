4ffceab0d0f3e5c6ad74dfa282b01f75d3495eda
KimberlyPeters <kimberlypeters468@gmail.com>
2023-05-09 05:05:22 -0700

    Betty compliant

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,37 +9,37 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current_node, *prev_node;
+	listint_t *new_node, *current_node, *prev_node;
 
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return NULL;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->n = number;
-    new_node->next = NULL;
+	new_node->n = number;
+	new_node->next = NULL;
 
-    if (!*head)
-    {
-        *head = new_node;
-        return new_node;
-    }
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    if (number < (*head)->n)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return new_node;
-    }
+	if (number < (*head)->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    current_node = *head;
-    while (current_node && number > current_node->n)
-    {
-        prev_node = current_node;
-        current_node = current_node->next;
-    }
+	current_node = *head;
+	while (current_node && number > current_node->n)
+	{
+		prev_node = current_node;
+		current_node = current_node->next;
+	}
 
-    new_node->next = current_node;
-    prev_node->next = new_node;
+	new_node->next = current_node;
+	prev_node->next = new_node;
 
-    return new_node;
+	return (new_node);
 }
