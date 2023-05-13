b5b1eb048d66d3aecf175998836339509039ee32
KimberlyPeters <kimberlypeters468@gmail.com>
2023-05-13 08:04:35 -0700

    adds 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    x1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    x2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    y1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    y2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    add = (x1 + y1, x2 + y2)
+    return add[0:2]
