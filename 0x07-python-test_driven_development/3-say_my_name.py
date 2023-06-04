418917eb0b4327e3d0d66cbd6a2e2b129cbdfae2
KimberlyPeters <kimberlypeters468@gmail.com>
2023-06-04 09:13:43 -0700

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,7 +9,7 @@ def say_my_name(first_name, last_name=""):
     Prints "My name is <first name> <last name>".
     Args:
         first_name: A string representing the first name.
-        last_name: (Optional) A string representing the last name. Defaults to an empty string.
+        last_name: (Optional) A string representing the last name.
     Raises:
         TypeError: If first_name or last_name is not a string.
     """
@@ -22,7 +22,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     # Print the name
-    if last_name:
-        print(f"My name is {first_name} {last_name}")
-    else:
-        print(f"My name is {first_name}")
+    print(f"My name is {first_name} {last_name}")
