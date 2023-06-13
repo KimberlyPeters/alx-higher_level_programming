d9ade96d003eb31313cf0edfa29a24478a95b36e
KimberlyPeters <kimberlypeters468@gmail.com>
2023-06-13 06:58:26 -0700

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle of size n.
+
+    Args:
+        n (int): The size of Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
